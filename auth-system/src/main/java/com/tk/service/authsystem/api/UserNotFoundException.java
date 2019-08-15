6284977b52521534e8dc2e26b5952cf2aa5ef3c3
Timur Karimov <timur.karimow.95@gmail.com>
2019-08-15 20:54:39 +0200

    Add UserDetailsService from Spring filter chain to fetch user data

--- auth-system/src/main/java/com/tk/service/authsystem/api/UserNotFoundException.java
+++ auth-system/src/main/java/com/tk/service/authsystem/api/UserNotFoundException.java
@@ -1,10 +1,11 @@
 package com.tk.service.authsystem.api;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "user not found")
-public class UserNotFoundException extends Exception {
+public class UserNotFoundException extends UsernameNotFoundException {
     public UserNotFoundException() {
         super("PersistedUser wasn't found");
     }
--- auth-system/src/main/java/com/tk/service/authsystem/security/WebSecurity.java
+++ auth-system/src/main/java/com/tk/service/authsystem/security/WebSecurity.java
@@ -1,18 +1,22 @@
 package com.tk.service.authsystem.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private UserDetailsServiceImpl userDetailsService;
 
-    public WebSecurity(BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public WebSecurity(BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailsServiceImpl userDetailsServiceImpl) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userDetailsService = userDetailsService;
     }
 
     @Override
@@ -23,6 +27,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable();
         http.headers().frameOptions().disable();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
 }
