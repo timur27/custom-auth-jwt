6f33661228e76da16a80075e9c2b1824161330a0
Timur Karimov <timur.karimow.95@gmail.com>
2019-08-25 21:39:02 +0200

    Implement Api-Gateway controller fiters

--- api-gateway/src/main/java/com/tk/service/apigateway/application/filter/ApiGatewayAuthFilter.java
+++ api-gateway/src/main/java/com/tk/service/apigateway/application/filter/ApiGatewayAuthFilter.java
@@ -1,16 +1,40 @@
 package com.tk.service.apigateway.application.filter;
 
+import com.tk.service.apigateway.ex.TokenNotProvidedException;
+import com.tk.service.apigateway.util.jwt.JWTokenHelper;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
 
 public class ApiGatewayAuthFilter extends HandlerInterceptorAdapter {
+    private static final String LOG_IN_PATH = "/login";
+    private static final String PRIVATE_KEY = "privateKey";
+    private JWTokenHelper jwTokenHelper = JWTokenHelper.getInstance();
+//    private Logger logger = LoggerFactory.getLogger(ApiGatewayAuthFilter.class);
+
     @Override
     public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception {
-        System.out.println("PRE handle");
+        if (req.getRequestURI().equals(LOG_IN_PATH)) {
+            return true;
+        } else {
+            claimToken(req, res, handler);
+        }
         return true;
+    }
+
+    private void claimToken(HttpServletRequest req, HttpServletResponse res, Object handler) {
+        String privateKeyHeaderValue = Optional.of(req.getHeader(PRIVATE_KEY))
+                .orElseThrow(() -> new TokenNotProvidedException("Token was not provided"));
+        try {
+            jwTokenHelper.claimKey(privateKeyHeaderValue);
+        } catch (Exception ex) {
+//            logger.error("JWT Token is expired or is incorrect");
+            System.out.println("JWT Token is expired or is incorrect");
+        }
     }
 
     @Override
