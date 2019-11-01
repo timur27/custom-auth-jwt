c264d21ef4f267434c6fc05495b8ece4f0994c5e
Timur Karimov <timur.karimow.95@gmail.com>
2019-11-01 21:55:51 +0100

    [TK-001] Add CORS to spring boot config

--- api-gateway/src/main/java/com/tk/service/apigateway/application/config/WebConfig.java
+++ api-gateway/src/main/java/com/tk/service/apigateway/application/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.tk.service.apigateway.application.config;
 
 import com.tk.service.apigateway.application.filter.ApiGatewayAuthFilter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new ApiGatewayAuthFilter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
